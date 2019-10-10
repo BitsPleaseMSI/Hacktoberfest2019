2765664cd9c2988997a6419ba5e1e21f4bbe70bc
himanta1012 <50390378+himanta1012@users.noreply.github.com>
2019-10-10 14:41:10 +0530

    Update e10.c

--- e10.c
+++ e10.c
@@ -6,7 +6,7 @@ int main()
 	int num1[10],i,avg,add=0;
 	printf("Enter number 10nos");
 	for(i=0;i<10;i++)
-    {scanf("%d",&num1[i]);
+     {scanf("%d",&num1[i]);
 	 add=add+num1[i];
     }
 	avg=add/10;
