74c97e6fab1895a35d698316e99884688427aa76
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2019-10-10 15:09:56 +0530

    ep2.cpp

--- e2.cpp
+++ e2.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+	using namespace std;
+
+	int main()
+	{
+		int n;
+		cin >> n;
+		if (n < 0)
+			cout << n << " is negative" << endl;
+		else if (n > 0)
+			cout << n << " is positive" << endl;
+
+		return 0;
+	}
