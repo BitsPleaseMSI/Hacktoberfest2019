01ce17a0e6ed008437339b326888bf654f864a0a
gabbar7777 <56385607+gabbar7777@users.noreply.github.com>
2019-10-10 14:22:01 +0530

    display 2d matrix

---
+++ e4.c
@@ -0,0 +1,20 @@
+#include<iostream.h>
+main()
+{
+int arr[2][2];
+for(i=0;i<=1;i++)
+  {
+   for(j=0;j<=1;j++)
+   {
+   cin>>"enter the elements of array">>arr[i][j];
+   }
+  }
+for(i=0;i<=1;i++)
+  {
+   for(j=0;j<=1;j++)
+   {
+   cout<<" the elements of array are"<<arr[i][j];
+   }
+  }
+  getch()
+  }
