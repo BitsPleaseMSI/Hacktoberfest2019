1a40191ba030d9108172eb205ef214d6c4fe2c7a
himanta1012 <50390378+himanta1012@users.noreply.github.com>
2019-10-10 23:36:14 +0530

    updated e8.cpp with sol. of QUE 8 (EASY)

---
+++ e8.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,sq,cu;
+    cout<<"Enter the number";
+    cin>>n;
+    sq=n*n;
+    cu=n*n*n;
+    cout<<"Square is "<<sq<<endl;
+    cout<<"Cube is "<<cu;
+    
+    return 0;
+}
