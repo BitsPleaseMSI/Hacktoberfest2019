1cf849930b0e9ea70b7d3951c9b94791811cc9fe
puneet2715 <55136802+puneet2715@users.noreply.github.com>
2019-10-10 14:34:46 +0530

    e8
    1st commit

---
+++ e8.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main()
+{
+    int a;
+    printf("Enter a Number: ");
+    scanf("%d",&a);
+    printf("%d square of %d",a*a,a);
+    printf("\n%d is cube of %d",a*a*a,a);   
+}
