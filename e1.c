b030377f1a057889e20a3d17f0f21e2b22a9ee48
AnirudhGosain <56385670+AnirudhGosain@users.noreply.github.com>
2019-10-10 14:48:30 +0530

    Update e1.c

---
+++ e1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+  
+    if(number % 2 == 0)
+        printf("%d is even.", number);
+    else
+        printf("%d is odd.", number);
+}
