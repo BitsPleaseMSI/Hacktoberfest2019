80871d0cbce1a04c05a01e5836338e7815fee16a
Your Father <50111518+samuel7james@users.noreply.github.com>
2019-10-10 14:26:12 +0530

    e2.cpp
    Second Commit by Samuel

---
+++ e2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int a;
+    cout<<"Enter the number to be checked"<<endl;
+    cin>>a;
+    if(a<0)
+    {
+        cout<<"The entered number is negative"<<endl;
+    }
+    else
+    cout<<"The entered number is positive"<<endl;
+}
