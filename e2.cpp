ed12e1539136789564f0105a77426e562a71c6ba
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2019-10-10 15:06:09 +0530

    Update e2.cpp

--- e2.cpp
+++ e2.cpp
@@ -1,17 +1,1 @@
 
-	#include <iostream>
-	#include <algorithm>
-
-	using namespace std;
-
-	int main()
-	{
-		int n;
-		cin >> n;
-		if (n < 0)
-			cout << n << " is negative" << endl;
-		else if (n > 0)
-			cout << n << " is positive" << endl;
-
-		return 0;
-	}
